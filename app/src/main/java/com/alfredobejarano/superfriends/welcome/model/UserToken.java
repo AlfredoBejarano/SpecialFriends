7b3ab6bc42511a486c644e1c798e8a5c6d6911a7
AlfredoBejarano <alfredocbejarano@gmail.com>
2018-06-26 14:20:56 -0500

    Adds missing documentation in UserToken.java

--- app/src/main/java/com/alfredobejarano/superfriends/welcome/model/UserToken.java
+++ app/src/main/java/com/alfredobejarano/superfriends/welcome/model/UserToken.java
@@ -13,10 +13,18 @@ public class UserToken {
      */
     private String value;
 
+    /**
+     * Retrieves the value of the User token.
+     * @return The value of the token.
+     */
     public String getValue() {
         return value;
     }
 
+    /**
+     * Sets a value for the user facebook token.
+     * @param value The new value for the token.
+     */
     public void setValue(String value) {
         this.value = value;
     }
