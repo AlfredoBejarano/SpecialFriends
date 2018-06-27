be4cea48f2f3171c9d1b26196f071138135cdc37
AlfredoBejarano <alfredocbejarano@gmail.com>
2018-06-27 12:07:42 -0500

    Adds missing documentation to CircularCallback

--- app/src/main/java/com/alfredobejarano/superfriends/common/view/CircularCallback.java
+++ app/src/main/java/com/alfredobejarano/superfriends/common/view/CircularCallback.java
@@ -1,4 +1,4 @@
-package com.alfredobejarano.superfriends.home.view;
+package com.alfredobejarano.superfriends.common.view;
 
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
@@ -9,24 +9,47 @@ import android.widget.ImageView;
 import com.alfredobejarano.superfriends.R;
 import com.squareup.picasso.Callback;
 
+/**
+ * This class defines a behaviour of drawing a retrieved image from Picasso as a circle.
+ * @author Alfredo Bejarano
+ */
 public class CircularCallback implements Callback {
+    /**
+     * ImageView to be drawn the image into.
+     */
     private ImageView mImageView;
 
+    /**
+     * Constructor that receives a reference to an {@link ImageView}.
+     * @param imageView The ImageView to be used to drawn the retrieved picture.
+     */
     public CircularCallback(ImageView imageView) {
         mImageView = imageView;
     }
 
+    /**
+     * This method is called when the image was retrieved successfully.
+     */
     @Override
     public void onSuccess() {
+        // Get the retrieved image from the ImageView.
         Bitmap imageBitmap = ((BitmapDrawable) mImageView.getDrawable()).getBitmap();
+        // Convert the Bitmap into a RoundedBitmap.
         RoundedBitmapDrawable imageDrawable = RoundedBitmapDrawableFactory.create(mImageView.getContext().getResources(), imageBitmap);
+        // Set the RoundedBitmap as a perfect circle.
         imageDrawable.setCircular(true);
+        // Set the drawable corner radius as the half of the image width and image height.
         imageDrawable.setCornerRadius(Math.max(imageBitmap.getWidth(), imageBitmap.getHeight()) / 2.0f);
+        // Render the drawable into the ImageView.
         mImageView.setImageDrawable(imageDrawable);
     }
 
+    /**
+     * Set a place holder drawable when the image was not retrieved correctly.
+     */
     @Override
     public void onError() {
+        // Draw the placeholder image.
         mImageView.setImageResource(R.mipmap.ic_launcher_round);
     }
 }
