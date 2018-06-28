2cca72f3970f21242afc15636b26a2bf987fe7fe
AlfredoBejarano <alfredocbejarano@gmail.com>
2018-06-27 19:58:01 -0500

    Adds missing phone number property to profile information

--- app/src/main/java/com/alfredobejarano/superfriends/profile/model/ProfileInformation.java
+++ app/src/main/java/com/alfredobejarano/superfriends/profile/model/ProfileInformation.java
@@ -7,5 +7,6 @@ public class ProfileInformation {
     public MutableLiveData<String> photo = new MutableLiveData<>();
     public MutableLiveData<String> birthday = new MutableLiveData<>();
     public MutableLiveData<Boolean> favorite = new MutableLiveData<>();
+    public MutableLiveData<String> phoneNumber = new MutableLiveData<>();
     public MutableLiveData<String> description = new MutableLiveData<>();
 }
